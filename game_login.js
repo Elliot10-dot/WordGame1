4c11c97250b82e0a4e27b8e6445eca5f932e99e4
Elliot10-dot <78664677+Elliot10-dot@users.noreply.github.com>
2021-12-18 11:56:02 -0600

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,6 @@
+function start(){
+    var player1name=document.getElementById("player1_name_input").value;
+    var player2name=document.getElementById("player2_name_input").value;
+    localStorage.setItem("player1_name", player1name);
+    localStorage.setItem("player2_name", player2name);
+    window.location="game_page.html";}
